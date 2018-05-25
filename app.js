5b03d07ee9f3620dc37fbf3b40d348ab38e6c750
Evan Kleiner <gkleinereva@gmail.com>
2018-05-25 16:10:37 -0700

    Project Finished

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet= require("helmet");
 
 var mongoose = require('mongoose');
 var mongoDB = process.env.MONGODB_URI || 'mongodb://Admin:Admin@ds133570.mlab.com:33570/gkleinerevaimagesearch';
@@ -21,6 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- models/search.js
+++ models/search.js
@@ -1,10 +1,17 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
 var searchSchema = new Schema({
 	term: {type: String, required: true},
 	timestamp: {type: Date, required: true},
+});
+
+searchSchema
+.virtual('dateTime')
+.get(function() {
+	return moment(this.timestamp).format('MMMM Do, YYYY, h:mm:ss a');
 });
 
 module.exports = mongoose.model('Search', searchSchema);
